4e26f61c3361997aaafe559b941cf47c3d96f125
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2022-08-31 22:46:01 +0530

    java file

---
+++ #9.java
@@ -0,0 +1,24 @@
+// constructor
+/* 
+Syntax:
+
+class className
+{ 
+   className() // constructor
+      {
+	    //constructor body
+	  }
+	
+}
+
+*/ 
+
+
+
+class Hello 
+  {
+     Hello()
+	 {
+	   System.out.println("this is the output in the constructor");
+	 }
+}
